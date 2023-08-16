ea4dc87beec93779086c0747943fef5ba83da69b
ABDULLAHRH1 <abdullahrh64@gmaiI.com>
2023-08-16 23:00:50 +0300

    great

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  images: {
+    // Configure your image options here, if needed
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
